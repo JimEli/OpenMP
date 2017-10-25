9ffdfef84cf9b6c196c6718d2babe9fca4e1fd37
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-10-25 09:13:10 -0600

    Update main.c

--- OpenMP/main.c
+++ OpenMP/main.c
@@ -1,6 +1,6 @@
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 #include <time.h> // Include for timing purposes.
 
 // C/C++ Preprocessor Definitions: _CRT_SECURE_NO_WARNINGS
@@ -26,17 +26,6 @@ void mxvWithoutOpenMP(int m, int n, double *a, double *b, double *c) {
 			a[i] += b[i*n + j] * c[j];
 	}
 }
-
-/*
-void funcA() {
-	printf("In funcA: this section is executed by thread %d\n",
-		omp_get_thread_num());
-}
-void funcB() {
-	printf("In funcB: this section is executed by thread %d\n",
-		omp_get_thread_num());
-}
-*/
 
 int main(int argc, char *argv[]) {
 	double *a, *b, *c;
@@ -79,51 +68,6 @@ int main(int argc, char *argv[]) {
 	free(a);
 	free(b);
 	free(c);
-/*
-#pragma omp parallel shared(n) private(i)
-	{
-#pragma omp for
-		for (i = 0; i<n; i++)
-			printf("Thread %d executes loop iteration %d\n",
-				omp_get_thread_num(), i);
-	} // End of parallel region
 
-#pragma omp parallel
-	{
-#pragma omp sections
-		{
-#pragma omp section
-			(void)funcA();
-#pragma omp section
-			(void)funcB();
-		} // End of sections block 
-	} // End of parallel region 
-*/
 	return(0);
 }
-
-#if 0
-//
-// Simplistic dot-product demonstartion.
-//
-#define SIZE 10000
-
-int main(void) {
-	volatile double sum;
-	volatile double a[SIZE], b[SIZE];
-	int i, n = SIZE;
-	
-	for (i = 0; i < n; i++) {
-		a[i] = i * 0.5;
-		b[i] = i * 2.0;
-	}
-
-	sum = 0;
-#pragma omp parallel for default(none) reduction(+:sum) 
-	for (i = 0; i < n; i++) {
-		sum = sum + a[i] * b[i];
-	}
-
-	printf("sum = %lf\n", sum);
-}
-#endif
